5e6bb7f7eef8b6978f7b6cf77e46491e53b7576a
muhammaddzulfiqar <63487462+muhammaddzulfiqar@users.noreply.github.com>
2020-09-18 19:06:24 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ $('.page-scroll').on('click', function(e) {
 
  e.preventDefault();
 });
+
+//about
+$(window).on('load', function() {
+	$('.pKiri').addClass('pMuncul');
+	$('.pKanan').addClass('pMuncul');
+});
+
+// parallax
+$(window).scroll(function() {
+	var wScroll = $(this).scrollTop();
+
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+	});
+});
